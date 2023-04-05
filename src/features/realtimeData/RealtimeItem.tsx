4e75918bdda97d222c8a0c37062b9da669ba59da
Samuel Njenga <snjenga704@gmail.com>
2023-04-05 10:23:34 +0300

    feat: add temperature chip component

--- src/features/realtimeData/RealtimeItem.tsx
+++ src/features/realtimeData/RealtimeItem.tsx
@@ -1,7 +1,8 @@
-import Button from "@mui/material/Button";
+import { LocationOn } from "@mui/icons-material";
+import { Avatar, Box, Chip, Divider, Stack } from "@mui/material";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
+import { grey } from "@mui/material/colors";
 import Typography from "@mui/material/Typography";
 import moment from "moment";
 import React from "react";
@@ -37,22 +38,44 @@ const RealtimeItem = ({ realtimeData }: RealtimeData) => {
       <Card sx={{ minWidth: 275 }}>
         <CardContent>
           <Typography variant="h5" component="div">
-            Name of area : {name} Current time :{" "}
-            {moment(localtime).format("MMMM Do YYYY, h:mm:ss a")}
+            Name of area : <LocationOn sx={{ color: grey[500] }} />
+            {name}, {country}
+          </Typography>
+          <Typography variant="h5" component="div">
+            <Chip
+              label={`The Current time is ${moment(localtime).format(
+                "MMMM Do YYYY, h:mm:ss a"
+              )}`}
+            />
           </Typography>
           <Typography variant="h5" component="div">
             Name of region : {region}
           </Typography>
           <Typography variant="h5" component="div">
-            Country name : {country}
-          </Typography>
-          <Typography variant="h5" component="div">
             Area's longitude : {lon} Area's latitude : {lat}
           </Typography>
         </CardContent>
-        <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions>
+      </Card>
+
+      <Card>
+        <Box sx={{ p: 2, display: "flex" }}>
+          <Avatar variant="rounded" src={icon} />
+          <Stack spacing={0.5}>
+            <Typography fontWeight={700}>{text}</Typography>
+          </Stack>
+        </Box>
+        <Divider />
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="flex-start"
+          sx={{ px: 2, py: 1, bgcolor: "background.default" }}
+        >
+          <Typography>The Current Temperature</Typography>
+          <Chip
+            label={`Temperature in celcius is ${temp_c} and the Temperature in Fahrenheit is ${temp_f}`}
+          />
+        </Stack>
       </Card>
     </div>
   );
